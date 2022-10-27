55e5315f311c8bc6eae1142d17bbbbdc4a3a7524
yangyuxin <yangyuxin0714@gmail.com>
2022-10-27 17:19:44 -0400

    created event model

---
+++ db/migrate/20221027211446_create_events.rb
@@ -0,0 +1,20 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :event_name
+      t.string :address
+      t.datetime :start_time
+      t.datetime :end_time
+      t.float :price
+      t.text :description
+      t.string :tag
+      t.string :pictures
+      t.integer :available_spots
+      t.integer :occupied_spots
+      t.integer :organizer_id
+      t.string :applicants_id
+
+      t.timestamps
+    end
+  end
+end
