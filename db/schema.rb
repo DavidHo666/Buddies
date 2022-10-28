1a6b5ba5bf3424bc84423e1860e088ff92e6ff72
Dawei He <dh3027@columbia.edu>
2022-10-27 23:11:12 -0400

    fix user migration bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_212916) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_28_030939) do
   create_table "events", force: :cascade do |t|
     t.string "event_name"
     t.string "address"
