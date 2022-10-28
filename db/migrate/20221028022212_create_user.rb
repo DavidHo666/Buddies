e5fee713085cfc33c4d365b458dbcd4c903a95b8
Dawei He <dh3027@columbia.edu>
2022-10-27 22:22:53 -0400

    create user migration

---
+++ db/migrate/20221028022212_create_user.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.text :bio
+      t.datetime :joined_date
+      t.timestamps
+    end
+  end
+end
+
