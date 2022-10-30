4027ff570198391736da0022c3bc7aa43b7e8002
yangyuxin <yangyuxin0714@gmail.com>
2022-10-30 11:06:07 -0400

    created user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :events
+  get 'events/index'
+  root 'events#index'
+  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "events#index"
 end
