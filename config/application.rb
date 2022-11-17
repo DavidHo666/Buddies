8e83d5ee73ff9000becb88819c7c080ffc777223
Dawei He <hdw623@hotmail.com>
2022-11-16 19:40:29 -0500

    change to EST timezone

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module Buddies
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
