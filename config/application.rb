13a76054d8de3ae37ee854773eed180ff8e1a4fe
Erisae <xiayuhan18@mails.ucas.ac.cn>
2022-12-01 18:50:21 -0500

    aplictaion

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,26 @@ module Buddies
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Central Time (US & Canada)"
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     # config.time_zone = 'Eastern Time (US & Canada)'
     # config.assets.initialize_on_precompile = false
+    config.action_mailer.default_url_options = { :host => 'IP ADDRESS HERE:3000' }
+  
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.default_url_options = { 
+    #   :host => '<your_url_here>', 
+    #   :protocol => 'http'
+    # }
+    config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'elysiaswroth@gmail.com',
+      :password             => 'yffkrsbtlkeknnzz',
+      :authentication       => 'login',
+      :enable_starttls_auto => true
+    }
   end
 end
