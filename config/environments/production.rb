e21e1e537092941943c220a6ff8a0fb0ecc010e5
Erisae <xiayuhan18@mails.ucas.ac.cn>
2022-12-01 18:42:44 -0500

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,15 +94,18 @@ Rails.application.configure do
   #remeber to change it to the actual domain name when deploying
   config.action_mailer.default_url_options = {:host => 'buddies-test.herokuapp.com' }
   
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
-    :address              => 'smtp.gmail.com',
-    :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => 'elysiaswroth@gmail.com',
-    :password             => 'yffkrsbtlkeknnzz',
-    :authentication       => 'login',
+    :address              => 'smtp.sendgrid.net',
+    :port                 =>  25,
+    :domain               => 'heroku.com',
+    :user_name            => ENV["SENDGRID_USERNAME"],
+    :password             => ENV["SENDGRID_PASSWORD"],
+    :authentication       => 'plain',
     :enable_starttls_auto => true
   }
 end
