377c72ee190c4a3a850fc6922d5694569b4b880e
Erisae <xiayuhan18@mails.ucas.ac.cn>
2022-12-01 19:06:03 -0500

    test

--- config/application.rb
+++ config/application.rb
@@ -20,21 +20,5 @@ module Buddies
     # config.eager_load_paths << Rails.root.join("extras")
     # config.time_zone = 'Eastern Time (US & Canada)'
     # config.assets.initialize_on_precompile = false
-    config.action_mailer.default_url_options = {:host => 'buddies-test.herokuapp.com' }
-  
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default :charset => "utf-8"
-  
-    config.action_mailer.smtp_settings = {
-      :address              => 'smtp.sendgrid.net',
-      :port                 =>  25,
-      :domain               => 'heroku.com',
-      :user_name            => ENV["SENDGRID_USERNAME"],
-      :password             => ENV["SENDGRID_PASSWORD"],
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
-    }
   end
 end
