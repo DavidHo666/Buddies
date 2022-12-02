f793fffaaa839cb156605d09da64b99b21b58ee0
Erisae <xiayuhan18@mails.ucas.ac.cn>
2022-12-01 23:58:28 -0500

    email donw

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
 class UserMailer < ApplicationMailer
-    default from: 'buddies <elysiasworth@gmail.com>'
+    default from: 'Buddies:Join Now <buddies.join.now@gmail.com>'
 
     def welcome_email
         @user = params[:user]
-        @url  = 'localhost:3000'
+        @url  = 'https://buddies4152.herokuapp.com/'
         mail(to: @user.email, subject: 'Welcome to buddies')
     end
 
     def full_notification
         @event = params[:event]
-        @url = 'localhost:3000'
+        @url = 'https://buddies4152.herokuapp.com/'
         
         mail(to: [@event.users.map(&:email), @event.user.email], subject: "Event #{@event.event_name} is full")
     end
