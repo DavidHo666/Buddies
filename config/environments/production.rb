5e1bd9f20bb1cdef52aec1e126f8664384e43db8
Erisae <xiayuhan18@mails.ucas.ac.cn>
2022-12-01 22:28:43 -0500

    emm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #remeber to change it to the actual domain name when deploying
-  # config.action_mailer.default_url_options = {:host => 'buddies-test.herokuapp.com' }
+
   
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
@@ -107,4 +107,20 @@ Rails.application.configure do
   #   :authentication       => 'plain',
   #   :enable_starttls_auto => true
   # }
+
+  config.action_mailer.default_url_options = {:host => 'buddies-test.herokuapp.com' }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'elysiasworth@gmail.com',
+    :password             => 'yffkrsbtlkeknnzz',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
 end
